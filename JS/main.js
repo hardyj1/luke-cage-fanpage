4864ba54d3148efbd6ea132521ce506e5f028d84
hardyj1 <hardyj1@seattleu.edu>
2016-10-20 07:59:33 -0400

    updated page with all assignment requirements fulfilled

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,30 @@
-$.get(
-				// get google developers youtube data api
+$(document).ready(function(){
+  
+  $.get(
 			"https://www.googleapis.com/youtube/v3/playlistItems", {
 				part: 'snippet',
-				// limit videos shown to 8
-				maxResults: 8, 
-				// YouTube playlist ID
-				playlistId: 'IQtchcBHzZ-mSHdPi5PLnVx2yTI2sF-1',
-				// Personal Dev Key
+				// Display 10 videos
+				maxResults: 10, 
+				// Luke Cage soundtrack playlist ID
+				playlistId: 'PLIQtchcBHzZ-mSHdPi5PLnVx2yTI2sF-1',
+				// Personal Google Dev key
 				key:'AIzaSyDsDSyMj78b8SLdvhcTnJed3q013pCmrz4'},
 				function(data){
 					$.each(data.items, function(i, item){
-						// Output variable to put results in soundtrack div
+						//Output variable
 						var output;
 						console.log(item);
 						videoTitle = item.snippet.title;
 						videoId = item.snippet.resourceId.videoId;
-						// retrieves videos from playlist ID
+						
 						output = '<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
-						// append results to soundtrack-videos div 
+						
+						// append to #soundtrack-videos 
 						$('#soundtrack-videos').append(output);
 						
 					});
-				});
+        });
+  
+  
+});  
+  
